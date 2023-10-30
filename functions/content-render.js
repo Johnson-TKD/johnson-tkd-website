ab078ec7c62625f0575128dfddcf9d039a8950b7
LesterYiu <lester.ws.yiu@gmail.com>
2023-10-30 16:56:11 -0400

    Syntax fix

--- functions/content-render.js
+++ functions/content-render.js
@@ -128,8 +128,8 @@ export async function getData() {
     let data;
       
     const client = createClient({
-      space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-      accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+      	space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+      	accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
     });
   
     try {
