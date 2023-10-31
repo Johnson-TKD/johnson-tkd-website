6c26ab32ca8d024e80cce2b2ca879806b1a5523a
LesterYiu <lester.ws.yiu@gmail.com>
2023-10-31 15:57:16 -0400

    Removed use client from layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,14 @@
-'use client'
-
 import './globals.css'
-import { ContextProvider } from '@/components/context-provider'
 
 export default function RootLayout({ children }) {
 
 	return (
 
-		<ContextProvider>
-			<html lang="en" className="scroll-smooth">
-				<body className="bg-brand-2">
-					{ children }
-				</body>
-			</html> 
-		</ContextProvider>
+		<html lang="en" className="scroll-smooth">
+			<body className="bg-brand-2">
+				{ children }
+			</body>
+		</html> 
 
 	);
 
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Header from '../components/header';
 import HtmlHead from '@/components/head';
 import { getData, contentRender } from '@/functions/content-render';
 import Footer from '@/components/footer';
+import { ContextProvider } from '@/components/context-provider'
 
 export default async function Home() {
 
@@ -11,35 +12,37 @@ export default async function Home() {
 
     return (
         
-        <main>
-            <HtmlHead />
-            <Header
-                {
-                    ...{
-                        data
+        <ContextProvider>
+            <main>
+                <HtmlHead />
+                <Header
+                    {
+                        ...{
+                            data
+                        }
                     }
+                />
+                { data?.fields?.sections &&
+
+                    data?.fields?.sections.map( ( section, key ) => {
+
+                        return ( 
+
+                            contentRender( section, key )
+
+                        )
+
+                    })
+
                 }
-            />
-            { data?.fields?.sections &&
-
-                data?.fields?.sections.map( ( section, key ) => {
-
-                    return ( 
-
-                        contentRender( section, key )
-
-                    )
-
-                })
-
-            }
-            <Footer
-                {
-                    ...{
-                        data
+                <Footer
+                    {
+                        ...{
+                            data
+                        }
                     }
-                }
-            />
-        </main>
+                />
+            </main>
+        </ContextProvider>
     )
 }
